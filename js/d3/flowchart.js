e4b9688c31975f57f7f3a20049dc23c549dcac38
selistym <selistym@protonmail.com>
2019-04-07 13:19:17 -0800

    completed 4-7

--- js/d3/flowchart.js
+++ js/d3/flowchart.js
@@ -1,4 +1,8 @@
 function draw_flowchart(json_file) {
+    //-------------------config variables-----------------------
+    var c_w = 80, c_h = 50, m = 40, //c_w:cell width, c_h: cell_height, m: margin
+        maps = [], nodes = [], links = [];        
+    //----------------------------------------------------------
 
     d3.select("svg").selectAll("*").remove();    
 
@@ -6,7 +10,7 @@ function draw_flowchart(json_file) {
         .attr("width", w)
         .attr("height", h)
         .append("g")
-        .attr("transform", "translate(" + w / 2 + ", 0)");
+        .attr("transform", "translate(" + w / 2 + ", 50)");
 
     svgContainer.append('defs').append("marker")
         .attr('id', 'arrowhead')
@@ -23,149 +27,171 @@ function draw_flowchart(json_file) {
         .style('stroke', 'none')
         .attr("class", "arrowHead");        
 
-    var behaviors = {};
-    behaviors.drag = d3.drag().on("start", dragstarted).on("drag", dragged).on("end", dragended);
-
-    var nodesArr = [],
-        linksArr = [],
-        top = 100,
-        h_margin = 50,
-        v_margin = 50,
-        r_w = 150,
-        r_h = 60;
-
     //load json data    
     jQuery.ajax({
         dataType: "json",
         url: "json/" + json_file,
         async: false,
         success: function (data) {
-            nodesArr = data.nodes;
-            nodesArr.forEach((n, i) => {
-                nodesArr[i] = {
+            nodes = data.nodes;
+            nodes.forEach((n, i) => {
+                nodes[i] = {
                     id: n.id,
                     category: n.category,
-                    pos_index: n.pos_index,
-                    level: n.level,
-                    cx: getCenterPosition(n.level, n.pos_index, n.category).cx,
-                    cy: getCenterPosition(n.level, n.pos_index, n.category).cy,
+                    i: n.i,
+                    j: n.j,
+                    pos: getPositions(n.i, n.j, n.category),
                     text: n.text
                 };
             });
-            linksArr = data.links;
-            linksArr.forEach((e, i) => {
-                linksArr[i] = {                    
-                    source: nodesArr.filter(n => n.id == e.from)[0],
-                    target: nodesArr.filter(n => n.id == e.to)[0]
+            links = data.links;
+            links.forEach((e, i) => {
+                links[i] = {                    
+                    source: nodes.filter(n => n.id == e.from)[0],
+                    target: nodes.filter(n => n.id == e.to)[0]
                 };
             });
+            console.log(nodes)
         }
     });
-    var max_left = d3.min(nodesArr.map(d => d.cx)),
-        max_right = d3.max(nodesArr.map(d => d.cx));    
     
+    var map_w = d3.max(nodes.map(d => d.i)) + 1,
+        map_h = d3.max(nodes.map(d => d.j)) + 1;
+    
+    //construct map
+    for(var i = 0; i < map_w; i++){
+        for(var j = 0; j < map_h; j++){
+            var cell = {
+                i: i,
+                j: j,
+                is_any: false
+            }
+            for(var p = 0; p < nodes.length; p++){
+                if(nodes[p].i == i && nodes[p].j == j){
+                    cell.is_any = true;
+                    break;
+                }
+            }
+            maps.push(cell);
+        }
+    }
+    //move graph to screen center
+    var flowContainer = svgContainer.append('g')
+        .attr('transform', 'translate(-' + map_w * (c_w + m) /2 + ', 0)');
+
     drawChart();
 
     function drawChart() {
 
-        var links = svgContainer
+        var linksGroup = flowContainer
             .selectAll(".link")
-            .data(linksArr);
-        var enteredLinks = links.enter().append('g').attr('class', 'link');
+            .data(links);
+        var enteredLinks = linksGroup.enter().append('g').attr('class', 'link');
         enteredLinks.append('path')
             .attr("d", d => getPath(d))            
             .attr("stroke-width", 3)
             .attr("fill", "none")
             .attr('marker-end', 'url(#arrowhead)');
 
-        var nodes = svgContainer
-            .selectAll(".node")
-            .data(nodesArr);
-        var enteredNodes = nodes.enter().append('g').attr('class', 'node');
+        var nodesGroup = flowContainer
+            .selectAll(".node") 
+            .data(nodes);
+        var enteredNodes = nodesGroup.enter().append('g').attr('class', 'node');                    
+
         enteredNodes
             .append("rect")
-            .attr("x", d => d.category != "" ? d.cx - r_h / 2 : d.cx - r_w / 2)
-            .attr("y", d => d.cy - r_h / 2)
-            .attr("rx", d => d.category != "" ? r_h : 0)
-            .attr("ry", d => d.category != "" ? r_h : 0)
-            .attr("width", d => d.category != "" ? r_h : r_w)
-            .attr("height", r_h)
-            .attr("fill", "#00a9c9")
-            .attr("opacity", 1);
+            .attr("x", d => d.pos.l.x)
+            .attr("y", d => d.pos.t.y)
+            .attr("rx", d => d.category == "circle" ? c_h : 0)
+            .attr("ry", d => d.category == "circle" ? c_h : 0)
+            .attr("width", d => d.category == "circle" ? c_h : c_w)
+            .attr("height", c_h)
+            .attr("fill", "#00a9c9");
+            
         enteredNodes
-            .attr('class', 'node-text')
             .append("text")
-            .attr("x", d => d.cx)
-            .attr("y", d => d.cy)
+            .attr("x", d => d.pos.c.x)
+            .attr("y", d => d.pos.c.y)
             .attr("text-anchor", "middle")
             .attr("alignment-baseline", "central")
             .text(d => d.text);
-
     }
-
-    //handler drag start event
-    function dragstarted(d) {
-
-    }
-    // handle dragging event
-    function dragged(d) {
-
-    }
-    //-------------------- handle drag end event ---------------
-    function dragended(d) {
-
-    }
-
-    function getPath(d) {        
-        var source = d.source,
-            target = d.target,
-            pathStr = '',
-            dir = source.pos_index > target.pos_index ? -1 : 1,
-            dis = Math.abs(source.pos_index - target.pos_index),
-            lvl_dis = Math.abs(source.level - target.level);
-
-        if (source.level == target.level) {
-            
-            pathStr = 'M' + (source.cx + dir * r_w / 2) + ',' + source.cy + 'l' + dir * v_margin/2 + ',0';
-            if(dis > 1){
-                pathStr += 'l0,' + (r_h / 2 + h_margin / 2) + 'l' + dir * (v_margin + r_w) + ',0l0,-' + (r_h / 2 + h_margin / 2);
+    
+    //--------------------Matrix helper functions-----------------------
+    function getPath(d) {  
+        var strPath = '';      
+        var src = d.source,
+            tar = d.target;
+        // rows
+        if(src.i == tar.i){
+            if(src.j > tar.j){
+                if(src.j - tar.j == 1){
+                    strPath = 'M' + src.pos.l.x + ',' + src.pos.l.y + 'L' + tar.pos.r.x + ',' + tar.pos.r.y;
+                }else{
+                    strPath = 'M' + src.pos.l.x + ',' + src.pos.l.y + 
+                            'l' + (-m/2) + ',0' + 
+                            'l0,' + (c_h + m)/2 + 
+                            'l' + (-(m + c_w) * (src.j - tar.j - 1)) + ',0' +
+                            'l0,' + (-(c_h + m)/2) + 
+                            'l' + (-m/2) + ',0';                    
+                }                
+            }else if(src.j < tar.j){
+                if(tar.j - src.j == 1){
+                    strPath = 'M' + src.pos.r.x + ',' + src.pos.r.y + 'L' + tar.pos.l.x + ',' + tar.pos.l.y;
+                }else{
+                    strPath = 'M' + src.pos.r.x + ',' + src.pos.r.y + 
+                            'l' + (m/2) + ',0' + 
+                            'l0,' + (c_h + m)/2 + 
+                            'l' + ((m + c_w) * (tar.j - src.j - 1)) + ',0' +
+                            'l0,' + (-(c_h + m)/2) + 
+                            'l' + (m/2) + ',0';
+                }
             }
-            pathStr += 'l' + dir * v_margin/2 + ',0';            
-        } else {            
-            pathStr = 'M' + source.cx + ',' + (source.cy + r_h / 2) + 'l0,' + h_margin / 2;
-            // if(lvl_dis == 1 && dis == 0 ){
-            //     pathStr += 'l' + (r_w + v_margin) * dir * dis + ',0';
-            // }else if(lvl_dis > 1 && dis > 0){
-            //     if(dir < 0){
-            //         pathStr += 'l' +  (max_left + r_w/2 + v_margin) * dir + ',0l0,' + lvl_dis * (h_margin + r_h) + 'l' + (max_left + r_w/2 + v_margin) * (-dir) + ',0';
-            //     }else{
-            //         pathStr += 'l' +  (max_right + r_w/2 + v_margin) * dir + ',0l0,' + lvl_dis * (h_margin + r_h) + 'l' + (max_left + r_w/2 + v_margin) * (-dir)  + ',0';
-            //     }
-            // }
-            pathStr += 'l0,' + h_margin / 2;
-            // console.log(pathStr)
-        }
-        
-        return pathStr;
-    }
-
-    function getCenterPosition(level, pos_index, category) { //type : start or ""
-        var cx, cy;
-        if (category != "") { // start
-            cx = 0;
-        } else {
-            if (pos_index < 0) {
-                cx = -(Math.abs(pos_index) * (r_w + v_margin));
-            } else if (pos_index > 0) {
-                cx = pos_index * (r_w + v_margin);
-            } else {
-                cx = 0;
+        }else{// src.i < tar.i            
+            if(src.j > tar.j){
+                strPath += 'M' + src.pos.b.x + ',' + src.pos.b.y +
+                        'l0,' + m/2 + 
+                        'l' + (-(c_w + m)) * (src.j - tar.j) + ',0';
+                if(tar.i - src.i > 1){
+                    strPath +=  'l' + (-(c_w + m)/2) + ',0' +
+                                'l0,' + (tar.i - src.i - 1) * (m + c_h) + 
+                                'l' + (c_w + m)/2 + ',0';                                 
+                }
+                strPath += 'l0,' + m/2;                
+            }else if(src.j < tar.j){
+                strPath += 'M' + src.pos.b.x + ',' + src.pos.b.y +
+                        'l0,' + m/2 + 
+                        'l' + ((c_w + m)) * (tar.j - src.j) + ',0';
+                if(tar.i - src.i > 1){
+                    strPath +=  'l' + ((c_w + m)/2) + ',0' +
+                                'l0,' + (tar.i - src.i - 1) * (m + c_h) + 
+                                'l' + (-(c_w + m)/2) + ',0';                                 
+                }
+                strPat += 'l0,' + m/2;                
+            }else{// src.j = tar.j
+                if(tar.i - src.i > 1){
+                    strPath = 'M' + src.pos.b.x + ',' + src.pos.b.y +
+                        'l0,' + m/2 + 
+                        'l' + ((c_w + m)/2) + ',0' +
+                        'l0,' + (tar.i - src.i) * (m + c_h) + 
+                        'l' + (-(c_w + m)/2) + ',0' +
+                        'l0,' + m/2;                    
+                }else{
+                    strPath = 'M' + src.pos.b.x + ',' + src.pos.b.y + 'L' + tar.pos.t.x + ',' + tar.pos.t.y;
+                }                
             }
         }
-        cy = top + level * (r_h + h_margin);
+        return strPath;
+    }  
+    function getPositions(i, j, category) { //category : circle or rect
+        var cx = j * (c_w + m) + c_w/2,
+            cy = i * (c_h + m) + c_h/2;
         return {
-            cx: cx,
-            cy: cy
+            l: { x: category == "circle" ? cx - c_h/2 : cx - c_w/2, y: cy},//left
+            r: { x: category == "circle" ? cx + c_h/2 : cx + c_w/2, y: cy},//right
+            t: { x: cx, y: cy - c_h/2},//top
+            b: { x: cx, y: cy + c_h/2},//bottom
+            c: { x: cx, y: cy}//center
         };
     }
 
